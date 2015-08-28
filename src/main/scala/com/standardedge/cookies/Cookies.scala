33b4c711918a71e226a2841399f14a42a461d05f
Matt Clifton <spartacus003@hotmail.com>
2015-08-28 14:04:12 -0700

    added set method

--- src/main/scala/com/standardedge/cookies/Cookies.scala
+++ src/main/scala/com/standardedge/cookies/Cookies.scala
@@ -1,9 +1,27 @@
 package com.standardedge.cookies
 
 import scala.scalajs.js
+import scala.scalajs.js.UndefOr
 import scala.scalajs.js.annotation.JSName
+
+trait Cookies {
+  def set(key: String, value: String): Cookies
+  def set(key: String, value: String, options: js.Dynamic): Cookies
+  def get(key: String): String
+}
 
 @JSName("Cookies")
 object Cookies extends js.Object {
-  def get(key: String): String = js.native
+  def set(key: String, value: String): Cookies = js.native
+  def set(key: String, value: String, options: js.Dynamic): Cookies = js.native
+  def get(key: String): UndefOr[String] = js.native
+}
+
+object Options {
+  def apply(path: UndefOr[String] = js.undefined,
+            domain: UndefOr[String] = js.undefined,
+            expires: UndefOr[Double] = js.undefined,
+            secure: UndefOr[Boolean] = js.undefined): js.Dynamic = {
+    js.Dynamic.literal("path" -> path, "domain" -> domain, "expires" -> expires, "secure" -> secure)
+  }
 }
